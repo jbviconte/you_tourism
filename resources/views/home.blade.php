42bde137d6f79288d0546bbe8c61a50910a100b2
Quentin <lorentzquentin@hotmail.fr>
2018-02-09 12:23:15 +0100

    acceuil

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,9 +1,12 @@
 @extends('layout/layout')
 
 @section('title')
-
+  You Tourism-Acceuil
 @endsection
 
 @section('content')
-
+  <h1>Acceuil</h1>
+<p>menu</p>
+<p>ici un slider image des lieux()</p>
+<p></p>
 @endsection
--- resources/views/layout/layout.blade.php
+++ resources/views/layout/layout.blade.php
@@ -6,6 +6,16 @@
   </head>
   <body>
 
+    <header id="menu">
+      {{-- <ul>
+        <li><a href="{{ route('home') }}">Acceuil</a></li>
+        <li><a href="{{ route('lieux') }}">Lieux
+           <ul><li><a href="{{ route('add_lieux') }}">Ajouter un lieux</a></li></ul>
+         </a></li>
+        <li><a href="{{ route('contact-view') }}">Contact</a></li>
+      </ul> --}}
+    </header>
+
     <div class="content">
       @yield('content')
     </div>
